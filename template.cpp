e9378d5ca6afb6e33c090ea10f7e2acafd891299
WangQing <wangqing6993@outlook.com>
2016-08-30 22:52:28 +0800

    template

---
+++ template.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+#define ll long long
+
+int main(){
+	freopen("A-large-practice.in.txt", "r", stdin);
+	freopen("A-large-practice.out.txt", "w", stdout);
+	int T;
+	cin >> T;
+	for(int count = 1; count < T + 1; count++){
+		ll res;
+		cin >> ;
+
+		
+		cout << "Case #" << count << ": " << res << endl;
+	}
+	return 0;
+}
